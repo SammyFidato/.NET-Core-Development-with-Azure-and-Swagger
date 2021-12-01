312b03bb2f14a1a904bf7edf13224d49abe2dc25
Sammy Fidato <sumansourav2@gmail.com>
2021-12-01 20:05:51 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,3 +1,2 @@
-$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
-az login -u "naman786sinha@gmail.com" -p "thinkMS123"
+az login --service-principal -u "f7d4aa74-439d-4e5b-a1e3-aea0d83ac987" -p "sBIa.zKN.sXYInDbIE0xGdmWzMJNR36-Q" --tenant "0c88fa98-b222-4fd8-9414-559fa424ce64"
 az apim api import -g aspdotnetswagger --service-name aspdotnetswagger --path itisfromapim --specification-url "https://aspdotnetswagger.azurewebsites.net/azure_swagger_dotnet/swagger/v1/swagger.json" --specification-format OpenApiJson --api-id "DotnetSwagger" --display-name "DotnetSwagger"
