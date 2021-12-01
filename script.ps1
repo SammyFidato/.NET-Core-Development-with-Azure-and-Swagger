a5b00282a3cd56418f4d97c0c1c3412afbf8dc1c
Sammy Fidato <sumansourav2@gmail.com>
2021-12-01 20:18:58 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,2 +1,3 @@
+az  account set --subscription faab79a5-d424-4699-b3ff-b0cf94ea9f90
 az login --service-principal -u "f7d4aa74-439d-4e5b-a1e3-aea0d83ac987" -p "sBIa.zKN.sXYInDbIE0xGdmWzMJNR36-QI" --tenant "0c88fa98-b222-4fd8-9414-559fa424ce64"
 az apim api import -g aspdotnetswagger --service-name aspdotnetswagger --path itisfromapim --specification-url "https://aspdotnetswagger.azurewebsites.net/azure_swagger_dotnet/swagger/v1/swagger.json" --specification-format OpenApiJson --api-id "DotnetSwagger" --display-name "DotnetSwagger"
