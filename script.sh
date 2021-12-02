d6c58db84f38e443e63abcc7ce1c2e88e6f5eb5d
Sammy Fidato <sumansourav2@gmail.com>
2021-12-03 00:22:21 +0530

    Delete script.sh

--- script.sh
+++
@@ -1,3 +0,0 @@
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-az login -u "naman786sinha@gmail.com" -p "thinkMS123"
-az apim api import -g aspdotnetswagger --service-name aspdotnetswagger --path itisfromapim --specification-url "https://aspdotnetswagger.azurewebsites.net/azure_swagger_dotnet/swagger/v1/swagger.json" --specification-format OpenApiJson --api-id "DotnetSwagger" --display-name "DotnetSwagger"
